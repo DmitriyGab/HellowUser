710c2d05cc883426c59351d82387f900220cb73c
Dr_Gab <gabrspam@gmail.com>
2023-04-15 01:38:35 +0300

    add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 5;
+int b = 6;
+int c = 7;
+int d = 9;
+int e = 3;
+int f = 2;
+int g = 7;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+if (g > max) max = g;
+
+Console.WriteLine("Максимальное значение "+max);
