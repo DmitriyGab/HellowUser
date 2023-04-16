50e1aa2d71ce0593ba835d531270bf5698ae00fc
Dr_Gab <gabrspam@gmail.com>
2023-04-16 22:28:58 +0300

    add Example010 Array Library

--- Example010_ArrayLibrary/Program.cs
+++ Example010_ArrayLibrary/Program.cs
@@ -5,13 +5,13 @@
 
     while(index < length)
     {
-    collection[index] = new Random().Next(1, 10);
+    collection[index] = new Random().Next(1, 10); // создание нового массива от 1 до 9
     index++;
     }
 
 }
 
-void PrintArray(int[] col)
+void PrintArray(int[] col) // вывод элементов массива
 {
     int count = col.Length;
     int position = 0;
@@ -22,7 +22,29 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find) // поиск нужного значения в массиве
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // Если в поиске введем число, которого не будет в созданном массиве, то значение -1 будет подсказывать, что это именно так
+    
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; // останавливает цикл при нахождении нужного значения
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int [10]; // создание нового массива с 10 элементами
 
 FillArray(array); // 
 PrintArray(array); // Печать массива
+Console.WriteLine(); // Пустая строка, чтобы отделить позицию искомого значения от значений массива PrintArray
+
+int pos = IndexOf(array, 2); // поиск нужного значения в массиве
+Console.WriteLine(pos); // вывод позиции нужного значения в массиве
