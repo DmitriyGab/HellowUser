3b01065c349352eec578c6d31ed62e3cc1c31fce
Dr_Gab <gabrspam@gmail.com>
2023-04-16 21:41:42 +0300

    add Example010 Array Library

---
+++ Example010_ArrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length; // определиние длины массива
+    int index = 0;
+
+    while(index < length)
+    {
+    collection[index] = new Random().Next(1, 10);
+    index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int [10]; // создание нового массива с 10 элементами
+
+FillArray(array); // 
+PrintArray(array); // Печать массива
