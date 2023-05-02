dc44f0393bf599f4eb3460c9baae4f846f7db48e
Dr_Gab <gabrspam@gmail.com>
2023-05-02 23:09:06 +0300

    add Example013 Recusion Algorithm 1

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -85,14 +85,14 @@ for (int i=2; i<10; i++)
 // ==========Работа с текстом
 // Дан текст. В тексте надовсе пробелы заменить черточками,
 // маленькие буквы "к" заменить большими буквами "К", 
-// а большие буквы "С" заменить маленькими буквами "С".
+// а большие буквы "С" заменить маленькими буквами "с".
 
 // Ясна ли задача?
 
 string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
                 + "ежели бы вас послали вместо нашего милого Винценгероде, "
                 + "вы бы взяли приступом согласие прусского короля."
-                + "Вы так красноречивы. Вы дадите мне чаю?";
+                + "Вы так красноречивы. Вы дадите мне чаю? Спасибо, Сэр";
 
 string Replace(string text, char oldValue, char newValue) // первая char - старый текст или символ. Вторая char - новый символ или текст, на которой надо заменить
 {
@@ -109,3 +109,47 @@ string Replace(string text, char oldValue, char newValue) // первая char -
 // string newText = Replace(text, " ", "/"); // ошибка, так как стоят кавычки, а должны стоять опострафы
 string newText = Replace(text, ' ', '/');
 Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+
+// Упорядочивание данных массива (от масимального к минимальному)
+
+int[] arr = {1, 2, 5, 8, 5, 3, 9, 2, 4};
+
+void PrintArray(int[] array) // Вывод массива
+{
+    int count = array.Length;
+
+    for (int i=0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
+
+void SelectionSort (int[] array)
+{
+    for (int i=0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j=i+1; j<array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+SelectionSort (arr);
+PrintArray (arr);
